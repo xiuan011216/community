100954ecccb6ea1f669b9d292f0b98fc2b3c951e
yubo <xuian1216@foxmail.com>
2022-09-10 15:53:21 +0800

    从配置文件中拿数据，实现从不同配置环境读取不同的配置文件

--- src/main/java/com/example/community_one/controller/AuthorizeController.java
+++ src/main/java/com/example/community_one/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.example.community_one.dto.AccessTokenDto;
 import com.example.community_one.dto.GitHubUser;
 import com.example.community_one.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,23 @@ public class AuthorizeController {
     @Autowired
     private GitHubProvider gitHubProvider;
 
+//    从配置文件中拿用@Value("${配置文件的key值}")
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name="state") String state){
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setCode(code);
-        accessTokenDto.setRedirect_uri("http://localhost:8885/callback");
+        accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
-        accessTokenDto.setClient_id("Iv1.3bf3e17e5f09748a");
-        accessTokenDto.setClient_secret("2c467b012d41206d5a4541559248956879e60fe9");
+        accessTokenDto.setClient_id(clientId);
+        accessTokenDto.setClient_secret(clientSecret);
 
         String accessToken = gitHubProvider.getAccessToken(accessTokenDto);
         GitHubUser user = gitHubProvider.getUser(accessToken);
