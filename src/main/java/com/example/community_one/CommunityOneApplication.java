1cf2aa7dd34f57793c1d5f7c488f7d9cd9aeb44c
yubo <xuian1216@foxmail.com>
2022-09-10 10:08:38 +0800

    项目创建测试

---
+++ src/main/java/com/example/community_one/CommunityOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.community_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityOneApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/community_one/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.community_one.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+@GetMapping("/index")
+public String index(@RequestParam(name = "name") String name, Model model){
+    model.addAttribute("name",name);
+    return "index";
+}
+}
