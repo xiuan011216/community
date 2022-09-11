1a731581271cfacf1ac7871db8837f2a9ed3bd6a
yubo <xuian1216@foxmail.com>
2022-09-11 13:28:59 +0800

    通过第三方登录完成用户数据储存到数据库

--- src/main/java/com/example/community_one/controller/AuthorizeController.java
+++ src/main/java/com/example/community_one/controller/AuthorizeController.java
@@ -1,7 +1,9 @@
 package com.example.community_one.controller;
 
+import com.example.community_one.model.User;
 import com.example.community_one.dto.AccessTokenDto;
 import com.example.community_one.dto.GitHubUser;
+import com.example.community_one.mapper.UserMapper;
 import com.example.community_one.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 //接收传过来的code和state
 @Controller
@@ -21,10 +24,16 @@ public class AuthorizeController {
 //    从配置文件中拿用@Value("${配置文件的key值}")
     @Value("${github.client.id}")
     private String clientId;
+
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
     @Value("${github.client.secret}")
     private String clientSecret;
+
+
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
@@ -38,10 +47,17 @@ public class AuthorizeController {
         accessTokenDto.setClient_secret(clientSecret);
 
         String accessToken = gitHubProvider.getAccessToken(accessTokenDto);
-        GitHubUser user = gitHubProvider.getUser(accessToken);
-       if (user!=null){
+        GitHubUser gitHubUser = gitHubProvider.getUser(accessToken);
+       if (gitHubUser!=null){
+           User user = new User();
+           user.setToken(UUID.randomUUID().toString());
+           user.setName(gitHubUser.getName());
+           user.setAccountId(String.valueOf(gitHubUser.getId()));
+           user.setGmtCreate(System.currentTimeMillis());
+           user.setGmtModified(user.getGmtCreate());
+           userMapper.insert(user);
 //           登录成功，创建cookie和session
-           request.getSession().setAttribute("user",user);
+           request.getSession().setAttribute("user",gitHubUser);
            return "redirect:/";
        }else {
 //           登录失败
---
+++ src/main/java/com/example/community_one/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.example.community_one.mapper;
+
+import com.example.community_one.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (account_id,name,token,gmt_create,gmt_modified) values (#{accountId},#{name},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
